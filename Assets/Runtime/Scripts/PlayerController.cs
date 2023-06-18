03f08bd74e41421f31e8f7a69dbb23a62c302fcb
Gustavo Almeida <gluiz.almeida@gmail.com>
2023-06-17 21:56:39 -0300

    Jump movement added

--- Assets/Runtime/Scripts/PlayerController.cs
+++ Assets/Runtime/Scripts/PlayerController.cs
@@ -7,16 +7,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horizontalSpeed = 10;
     [SerializeField] private float forwardSpeed = 10;
     [SerializeField] private float laneWidth = 4;
+    [SerializeField] private float jumpMaxHeight = 4;
+    [SerializeField] private float jumpMaxDistance = 4;
 
     public float RightLaneBound => _initialPosition.x + laneWidth;
     public float LeftLaneBound => _initialPosition.x - laneWidth;
 
+    public bool IsJumping { get; private set; }
+
     private Vector3 _initialPosition;
     private float _targetPositionX;
+    private float _jumpStartZ;
 
     private void Awake() 
     {
-        _initialPosition = transform.position;
+        _initialPosition = transform.position;        
     }
 
     private void Update()
@@ -26,6 +31,7 @@ public class PlayerController : MonoBehaviour
         var position = transform.position;
         position.x = ProcessLaneMovement();
         position.z = ProcessForwardMovement();
+        position.y = ProcessJumpMovement();
 
         transform.position = position;        
     }
@@ -42,6 +48,12 @@ public class PlayerController : MonoBehaviour
             _targetPositionX = transform.position.x - laneWidth;
         }
 
+        if (Input.GetKeyDown(KeyCode.W) && !IsJumping)
+        {
+           IsJumping = true;
+           _jumpStartZ = transform.position.z;
+        }
+
         _targetPositionX = Mathf.Clamp(_targetPositionX, LeftLaneBound, RightLaneBound);
     }
 
@@ -54,4 +66,27 @@ public class PlayerController : MonoBehaviour
     {
         return transform.position.z + forwardSpeed * Time.deltaTime;
     }
+
+    private float ProcessJumpMovement()
+    {
+        float deltaY = 0;
+
+        if (IsJumping)
+        {
+            var jumpCurrentProgress = transform.position.z - _jumpStartZ;
+            var jumpPercent = jumpCurrentProgress / jumpMaxDistance;
+
+            if (jumpPercent < 1)
+            {
+                deltaY = Mathf.Sin(Mathf.PI * jumpPercent) * jumpMaxHeight;
+            }
+            else
+            {
+                IsJumping = false;
+            }
+        }
+
+        return _initialPosition.y + deltaY;
+    }
+
 }
